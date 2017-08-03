d5accfa017de0730d6e870175227fbddce398f71
Andrew White <andrew.white@rochester.edu>
2017-08-03 07:36:18 -0400

    Added client receiver

--- Assets/Scripts/Communication/CommClient.cs
+++ Assets/Scripts/Communication/CommClient.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using NetMQ;
 using Google.Protobuf;
+using NetMQ.Sockets;
+using System.Threading.Tasks;
 
-public class CommClient : MonoBehaviour {
+namespace Rochester.Physics.Communication
+{
+    public class CommClient : MonoBehaviour
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private RequestSocket client;
+        private NetMQPoller poller;
+        private TaskCompletionSource<byte[]> responseTask;
+
+        public string server_uri = "@tcp://*:5000";
+
+        // Use this for initialization
+        void Start()
+        {
+            client = new RequestSocket(server_uri);
+            poller = new NetMQPoller { client };
+            //set-up event to add to task
+            responseTask = new TaskCompletionSource<byte[]>();
+            client.ReceiveReady += (s, a) => {
+                responseTask.SetResult(a.Socket.ReceiveFrameBytes());
+            };
+            //start polling thread
+            poller.RunAsync();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (responseTask.Task.IsCompleted)
+            {
+                GameState state = GameState.Parser.ParseFrom(responseTask.Task.Result);
+                System.Console.WriteLine("Received message {0}", state.Time);
+                //this is how you reset?
+                responseTask = new TaskCompletionSource<byte[]>();
+            }
+        }
+    }
+
 }
