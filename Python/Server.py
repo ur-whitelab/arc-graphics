5f76634a759d0a1a0aabcce78a78431bf72cfd66
Andrew White <andrew.white@rochester.edu>
2017-08-09 06:01:39 -0400

    Added more logging for python server

--- Python/Server.py
+++ Python/Server.py
@@ -1,9 +1,15 @@
-dimport zmq, zmq.asyncio, asyncio
+import zmq
+import zmq.asyncio
+import asyncio
 import State_pb2
 
 class StateServer:
+
+    connected = False
+
     def __init__(self, uri):
         self.ctx = zmq.asyncio.Context()
+        print('Opening PUB Socket on {}'.format(uri))
         self.sock = self.ctx.socket(zmq.PUB)
         self.sock.bind(uri)
         self.state = State_pb2.StructuresState()
@@ -16,25 +22,34 @@ class StateServer:
             a.position.append(i * 10.0)
             a.position.append(0.0)
 
-    async def update_gamestate(self):    
-        self.state.time += 1        
+    async def update_gamestate(self):
+        self.state.time += 1
         for a in self.state.structures:
-            a.position[1] += 0.01
-        await asyncio.sleep(0.02)
+            a.position[1] += 1
+        await asyncio.sleep(1)
         return self.state
-        
+
     async def pubLoop(self):
+        counter = 0
         while True:
             state = await self.update_gamestate()
-            self.sock.send(state.SerializeToString())           
+            self.sock.send(state.SerializeToString())
+            if(not self.connected):
+                self.connected = True
+                print('Socket has sent first packet')
+            counter += 1
+            if counter % 100 == 0:
+                print('Packet {} sent'.format(counter))
 
-def main():
+
+def main(host='*', port='5000'):
     zmq.asyncio.install()
     server = StateServer('tcp://*:5000')
+    print('Starting server at {} on port {}'.format(host, port))
     loop = asyncio.get_event_loop()
     loop.run_until_complete(server.pubLoop())
     loop.close()
-        
+
 
 
 
